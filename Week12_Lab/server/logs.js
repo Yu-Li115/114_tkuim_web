0477490059ce27c7d0fe04e04c7dbbf82bcc91b4
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-12-31 15:24:13 +0800

    1

--- Week12_Lab/server/logs.js
+++
@@ -1,5 +0,0 @@
-export function logMiddleware(req, res, next) {
-  const user = req.user?.email || 'guest';
-  console.log(`[${new Date().toISOString()}] ${user} ${req.method} ${req.originalUrl}`);
-  next();
-}
--- Week12_Lab/server/src/middleware/log.js
+++
@@ -1,6 +0,0 @@
-import { logAction } from '../../utils/logger.js'; 
-
-export const logActionMiddleware = (req, res, next) => {
-    req.logAction = logAction; 
-    next();
-};
